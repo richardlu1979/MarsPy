485d55e5b481270daebf4d9330f7f79e312c1a19
luchao <295702488@qq.com>
2018-03-30 09:25:57 +0800

    更新test

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 #hello world
 
 import tensorflow as ts 
+import time
+from time import sleep
 
 print(ts.__version__)
 
 print('Hello!')
+
+a=0
+while 1:
+   a=a+10
+   if a>300 :
+       print('close')
+       break
+    #this add mac open dialog
+
+
